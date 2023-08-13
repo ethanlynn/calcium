2ca47d70c29eefa24601d00a4077c25429ee024b
Ethan Lynn <ethanlynn0709@gmail.com>
2023-08-12 23:35:26 -0400

    feat: updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 </script>
 
-<style>
-</style>
+<div class="bg-slate-100 w-40 h-40">
+  <input type="text" />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx,svelte}"],
   theme: {
     extend: {},
   },
